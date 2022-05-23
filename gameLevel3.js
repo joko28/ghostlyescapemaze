32085e0662eb81f8700c91773bae81a8408c49eb
jnjoko <56281870+joko28@users.noreply.github.com>
2022-05-22 20:16:48 -0700

    add in

---
+++ gameLevel3.js
@@ -0,0 +1,22 @@
+function gameLevel3() {
+
+walls3 = new Group();
+
+//border wall
+wall2[0] = createSprite(300, 50, 600, 20); //top
+wall2[0].shapeColor = color(153, 51, 255);
+walls2.add(wall2[0]);
+
+wall2[1] = createSprite(10, 280, 20, 440); //left
+wall2[1].shapeColor = color(153, 51, 255);
+walls2.add(wall2[1]);
+
+wall2[2] = createSprite(590, 240, 20, 380); //right
+wall2[2].shapeColor = color(153, 51, 255);
+walls2.add(wall2[2]);
+
+wall2[3] = createSprite(300, 490, 600, 20); //bottom
+wall2[3].shapeColor = color(153, 51, 255);
+walls2.add(wall2[3]);
+
+}
